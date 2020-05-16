2e82604aa590bced058c3c47b3919403ca4ef47a
martha dunne <mabdunne@gmail.com>
2020-05-16 00:38:15 +0000

    lowercase text and remove stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import sys
+import string
+
+from sklearn.feature_extraction import stop_words
+
+stopwords = set(stop_words.ENGLISH_STOP_WORDS)
 
 # get all lines from stdin
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+    # remove leading and trailing whitespace and make lowercase
+    line = line.strip().lower().translate(None, string.punctuation)
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stopwords:
+            print '%s\t%s' % (word, "1")
